61815a367c4373fa4dc1757ffe331dc6ced4dd8d
lxz0503 <lxz_20081025@163.com>
2019-02-15 00:06:44 +0800

    aa

--- login_page.py
+++ login_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from .base import Page
+from lxz_python.base import Page
 
 
 class LoginPage(Page):
@@ -9,9 +9,11 @@ class LoginPage(Page):
     username_loc = (By.ID, 'u')
     password_loc = (By.ID, 'p')
     submit_loc = (By.ID, 'login_button')
+    error_name = (By.XPATH, '//*[@id="err_m"')
 
     def type_username(self, username):
-        self.find_element(*self.username_loc).send_keys(username)
+        #self.find_element(*self.username_loc).send_keys(username)
+        self.find_element(*self.error_name).send_keys(username)
 
     def type_password(self, password):
         self.find_element(*self.password_loc).send_keys(password)
