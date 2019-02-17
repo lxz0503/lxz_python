6cba8c62273650075c834a7e08739f601e93c305
lxz0503 <lxz_20081025@163.com>
2019-02-17 20:10:28 +0800

    bdd

--- BDD/test/features/steps.py
+++ BDD/test/features/steps.py
@@ -4,12 +4,12 @@ from lettuce import *
 @step('I have the number (\d+)')
 def have_the_number(step, number):
     world.number = int(number)
-    print (world.number)
+    #print (world.number)
 
 @step('I compute its factorial')
 def compute_its_fatorial(step):
     world.number = factorial(world.number)
-    print(world.number)
+    #print(world.number)
 
 @step('I see the number (\d+)')
 def check_number(step, expected):
